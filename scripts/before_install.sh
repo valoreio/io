4bc942b40deb3f37dab863ee6deb60904ecc675e
Marcos Barranco <marcosaureliobarranco@gmail.com>
2020-01-29 12:22:56 -0300

    setups

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-if [[ "$UID" –ne "$ROOT_UID" ]] ; then
-    echo "It must be root to run this bash script."
-    exit 1
-else
-    rm /var/www/django/contatoproj/templates/index.html
-    exit 0
-fi
+# if [[ "$UID" –ne "$ROOT_UID" ]] ; then
+#    echo "It must be root to run this bash script."
+#    exit 1
+#else
+rm /var/www/django/contatoproj/templates/index.html
+exit 0
+#fi
 
